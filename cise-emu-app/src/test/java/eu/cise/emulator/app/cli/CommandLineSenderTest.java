f4599ff667149ac9533ab1604be8918cc2007a0d
Gian Carlo Pace <giancarlo.pace@gmail.com>
2019-06-26 18:24:41 +0200

    Test is working

--- cise-emu-app/src/test/java/eu/cise/emulator/app/cli/CommandLineSenderTest.java
+++ cise-emu-app/src/test/java/eu/cise/emulator/app/cli/CommandLineSenderTest.java
@@ -5,7 +5,6 @@ import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.github.tomakehurst.wiremock.stubbing.ServeEvent;
 import eu.cise.emulator.app.CiseEmuApplication;
-import eu.cise.emulator.app.context.SimConfig;
 import eu.cise.servicemodel.v1.message.Message;
 import eu.cise.signature.SignatureService;
 import eu.cise.signature.SignatureServiceBuilder;
@@ -24,13 +23,12 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*;
 public class CommandLineSenderTest {
 
 
+    @Rule
+    public WireMockRule server
+            = new WireMockRule(WireMockConfiguration.options().port(64738), true);
     private WireMock mockCiseNode;
     private String simappPropertiesPath;
     private String messagePushPath;
-
-    @Rule
-    public WireMockRule server = new WireMockRule(WireMockConfiguration.options().port(64738), true);
-
 
     @Before
     public void setup() {
@@ -43,45 +41,48 @@ public class CommandLineSenderTest {
     @Test
     public void is_sending_a_message_from_cli() throws Exception {
         server.stubFor(post(urlEqualTo("/mock-node"))
-                .willReturn(aResponse()
-                        .withHeader("Content-Type", "text/xml")
-                        .withBody("<Push />")));
-        CiseEmuApplication.main(new String[]{"sender", "-c" , simappPropertiesPath,"-s", messagePushPath});
+                               .willReturn(aResponse()
+                                                   .withHeader("Content-Type", "text/xml")
+                                                   .withBody("<Push />")));
+        CiseEmuApplication.main(new String[]{"sender", "-c", simappPropertiesPath, "-s",
+                messagePushPath});
     }
 
     @Test
     public void is_sending_correct_applicationxml_message_from_cli() throws Exception {
         server.stubFor(post(urlEqualTo("/mock-node"))
-                .willReturn(aResponse()
-                        .withHeader("Content-Type", "text/xml")
-                        .withBody("<Push />")));
-        CiseEmuApplication.main(new String[]{"sender", "-c" , simappPropertiesPath,"-s", messagePushPath});
-        server.verify(postRequestedFor(urlEqualTo("/mock-node")).withHeader("Content-Type", equalTo("application/xml")));
+                               .willReturn(aResponse()
+                                                   .withHeader("Content-Type", "text/xml")
+                                                   .withBody("<Push />")));
+        CiseEmuApplication.main(new String[]{"sender", "-c", simappPropertiesPath, "-s",
+                messagePushPath});
+        server.verify(postRequestedFor(urlEqualTo("/mock-node")).withHeader("Content-Type",
+                                                                            equalTo("application/xml")));
     }
+
     @Test
     public void is_sending_correct_signed_message_from_cli() throws Exception {
         server.stubFor(post(urlEqualTo("/mock-node"))
-                .willReturn(aResponse()
-                        .withHeader("Content-Type", "text/xml")
-                        .withBody("<Push />")));
-        CiseEmuApplication.main(new String[]{"sender", "-c" , simappPropertiesPath,"-s", messagePushPath});
+                               .willReturn(aResponse()
+                                                   .withHeader("Content-Type", "text/xml")
+                                                   .withBody("<Push />")));
+        CiseEmuApplication.main(new String[]{"sender", "-c", simappPropertiesPath, "-s",
+                messagePushPath});
         List<ServeEvent> allServeEvents = getAllServeEvents();
-        ServeEvent test= allServeEvents.get(allServeEvents.size()-1);
-        String contentBodyString=test.getRequest().getBodyAsString();
+        ServeEvent test = allServeEvents.get(allServeEvents.size() - 1);
+        String contentBodyString = test.getRequest().getBodyAsString();
         XmlMapper refmapper = new DefaultXmlMapper();
-        Message aMessage = refmapper.<Message>fromXML(contentBodyString);
-        SignatureServiceBuilder signBuilder = SignatureServiceBuilder.newSignatureService(refmapper);
+        Message aMessage = refmapper.fromXML(contentBodyString);
+        SignatureServiceBuilder signBuilder
+                = SignatureServiceBuilder.newSignatureService(refmapper);
 
-        //SimConfig config = SimConfig.createMyConfig(simappPropertiesPath.);
-
-        String keyStoreFileName = "private.jks", certificatePassword = "cisecise", certificateKey = "sim",keyStorePassword = "cisecise", counterpartCertificateKey = "tpz";
-                    SignatureService signature = signBuilder
-                    .withKeyStoreName((String) keyStoreFileName)
-                    .withKeyStorePassword((String) certificatePassword)
-                    .withPrivateKeyAlias((String) certificateKey)
-                    .withPrivateKeyPassword((String) keyStorePassword)
-                    .build();
-            signature.verify(aMessage);
+        SignatureService signature = signBuilder
+                .withKeyStoreName("adaptor.jks")
+                .withKeyStorePassword("cisecise")
+                .withPrivateKeyAlias("sim1-node01.node01.eucise.fr")
+                .withPrivateKeyPassword("cisecise")
+                .build();
+        signature.verify(aMessage);
 
     }
 
@@ -89,16 +90,17 @@ public class CommandLineSenderTest {
     @Test
     public void is_sending_a_valid_message_from_cli() throws Exception {
         server.stubFor(post(urlEqualTo("/mock-node"))
-                .willReturn(aResponse()
-                        .withHeader("Content-Type", "text/xml")
-                        .withBody("<Push />")));
+                               .willReturn(aResponse()
+                                                   .withHeader("Content-Type", "text/xml")
+                                                   .withBody("<Push />")));
 
 
-        CiseEmuApplication.main(new String[]{"sender", "-c" , simappPropertiesPath,"-s", messagePushPath});
+        CiseEmuApplication.main(new String[]{"sender", "-c", simappPropertiesPath, "-s",
+                messagePushPath});
     }
 
     @After
-    public void teardown(){
+    public void teardown() {
         WireMock.resetAllRequests();
     }
 
