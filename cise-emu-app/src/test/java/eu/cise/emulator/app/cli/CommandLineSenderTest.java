564d3286292b8eab760a04047a663783bdfe94ae
Gian Carlo Pace <giancarlo.pace@gmail.com>
2019-06-26 16:59:09 +0200

    Server is responding

--- cise-emu-app/src/test/java/eu/cise/emulator/app/cli/CommandLineSenderTest.java
+++ cise-emu-app/src/test/java/eu/cise/emulator/app/cli/CommandLineSenderTest.java
@@ -1,9 +1,14 @@
 package eu.cise.emulator.app.cli;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import eu.cise.emulator.app.CiseEmuApplication;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 
 public class CommandLineSenderTest {
@@ -13,9 +18,12 @@ public class CommandLineSenderTest {
     private String simappPropertiesPath;
     private String messagePushPath;
 
+    @Rule
+    public WireMockRule server = new WireMockRule(WireMockConfiguration.options().port(64738), false);
+
+
     @Before
     public void setup() {
-        mockCiseNode = new WireMock("http", "localhost", 64738);
 
         simappPropertiesPath = getClass().getResource("/simapp.properties").getPath();
         messagePushPath = getClass().getResource("/AisModifiedPush.xml").getPath();
@@ -24,6 +32,12 @@ public class CommandLineSenderTest {
 
     @Test
     public void is_sending_a_message_from_cli() throws Exception {
+        server.stubFor(post(urlEqualTo("/mock-node"))
+                        .willReturn(aResponse()
+                                            .withHeader("Content-Type", "text/xml")
+                                            .withBody("<Push />")));
+
+
         CiseEmuApplication.main(new String[]{"sender", "-c" , simappPropertiesPath,"-s", messagePushPath});
     }
 
