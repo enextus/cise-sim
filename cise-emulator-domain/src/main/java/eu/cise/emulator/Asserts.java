12ae1863cfdac3da6f1cd6a11448d4156f6d74a4
Gian Carlo Pace <giancarlo.pace@gmail.com>
2019-09-13 18:16:07 +0200

    OCNET-154 GREEN: null param will result in a SendParamNullEx

---
+++ cise-emulator-domain/src/main/java/eu/cise/emulator/Asserts.java
@@ -0,0 +1,81 @@
+package eu.cise.emulator;
+
+import java.lang.reflect.InvocationTargetException;
+
+@SuppressWarnings("ALL")
+public class Asserts {
+
+    public static <T extends Exception> void asserts(boolean expression) throws T {
+        asserts(expression, null);
+    }
+
+    public static <T extends Exception> void asserts(boolean expression, String errorMessage,
+                                                     Object... args) throws T {
+        if (!expression) {
+            generateException(EmulatorEx.class, errorMessage, args);
+        }
+    }
+
+    public static <O, T extends Exception> O notNull(O reference) throws T {
+        return notNull(reference, null);
+    }
+
+    public static <O, T extends Exception> O notNull(O reference,
+                                                     String errorMessage,
+                                                     Object... args) throws T {
+        if (reference == null) {
+            generateException(EmulatorEx.class, errorMessage, args);
+        }
+        return reference;
+    }
+
+    public static <O, T extends Exception> O notNull(O reference, Class<T> ex) throws T {
+        if (reference == null) {
+            generateException(ex, null);
+        }
+        return reference;
+    }
+
+    public static <O, T extends Exception> O instanceOf(O reference, Class<?> wantedClass) throws T {
+        return instanceOf(reference, wantedClass, null);
+    }
+
+    public static <O, T extends Exception> O instanceOf(O reference, Class<?> wantedClass,
+                                                        String errorMessage, Object... args) throws T {
+        if (!wantedClass.isAssignableFrom(reference.getClass())) {
+            generateException(EmulatorEx.class, errorMessage, args);
+        }
+        return reference;
+    }
+
+    public static <O, T extends Exception> O notInstanceOf(O reference, Class<?> wantedClass,
+                                                           Class<T> exClass) throws T {
+        return notInstanceOf(reference, wantedClass, null);
+    }
+
+    public static <O, T extends Exception> O notInstanceOf(O reference, Class<?> wantedClass,
+                                                           String errorMessage, Object... args) throws T {
+        if (wantedClass.isAssignableFrom(reference.getClass())) {
+            generateException(EmulatorEx.class, errorMessage, args);
+        }
+
+        return reference;
+    }
+
+    private static <T extends Exception> void generateException(Class<T> exClass,
+                                                                String errorMessage,
+                                                                Object... args) throws T {
+        try {
+            if (errorMessage == null) {
+                throw exClass.newInstance();
+            } else {
+                throw exClass.getDeclaredConstructor(String.class, Object[].class).newInstance(errorMessage, args);
+            }
+        } catch (InstantiationException |
+                NoSuchMethodException |
+                InvocationTargetException |
+                IllegalAccessException e) {
+            e.printStackTrace(); // TODO handle it better
+        }
+    }
+}
--- cise-emulator-domain/src/main/java/eu/cise/emulator/DefaultEmulatorEngine.java
+++ cise-emulator-domain/src/main/java/eu/cise/emulator/DefaultEmulatorEngine.java
@@ -2,14 +2,17 @@ package eu.cise.emulator;
 
 import eu.cise.servicemodel.v1.message.Acknowledgement;
 import eu.cise.servicemodel.v1.message.Message;
-
+import static eu.cise.emulator.Asserts.*;
 public class DefaultEmulatorEngine implements EmulatorEngine {
 
     @Override
     public <T extends Message> T prepare(T message, SendParam param) {
+        notNull(param, SendParamNullEx.class);
+
         message.setRequiresAck(param.isRequiresAck());
         message.setMessageID(param.getMessageId());
         message.setCorrelationID(param.getCorrelationId());
+
         return message;
     }
 
