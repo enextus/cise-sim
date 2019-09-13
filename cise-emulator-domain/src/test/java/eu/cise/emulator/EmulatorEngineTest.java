9d4da13ebb6a597def161d2fa340e05e28ad7d74
Gian Carlo Pace <giancarlo.pace@gmail.com>
2019-09-13 16:41:31 +0200

    OCNET-154 New test of the messageId overriding

--- cise-emulator-domain/src/test/java/eu/cise/emulator/EmulatorEngineTest.java
+++ cise-emulator-domain/src/test/java/eu/cise/emulator/EmulatorEngineTest.java
@@ -1,6 +1,7 @@
 package eu.cise.emulator;
 
 import eu.cise.servicemodel.v1.message.Push;
+import org.junit.Before;
 import org.junit.Test;
 
 import static eu.eucise.helpers.PushBuilder.newPush;
@@ -11,18 +12,32 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class EmulatorEngineTest {
 
+    private EmulatorEngine engine;
+
+    @Before
+    public void before() {
+        engine = new DefaultEmulatorEngine();
+    }
 
     @Test
-    public void it_substitutes_the_requireAck() {
-        EmulatorEngine engine = new DefaultEmulatorEngine();
-
+    public void it_substitutes_the_requiresAck() {
         Push actual = newPush().isRequiresAck(false).build();
 
-        SendParam paramTrueAck = new SendParam(true, "id", "id");
+        SendParam paramTrueAck = new SendParam(true, "n/a", "n/a");
 
         Push expected = engine.prepare(actual, paramTrueAck);
 
         assertThat(expected.isRequiresAck()).isTrue();
+    }
 
+    @Test
+    public void it_substitutes_the_messageId() {
+        Push actual = newPush().id("to-be-overridden").build();
+
+        SendParam paramMsgId = new SendParam(false, "new-message-id", "n/a");
+
+        Push expected = engine.prepare(actual, paramMsgId);
+
+        assertThat(expected.getMessageID()).isEqualTo("new-message-id");
     }
 }
