41c1fc651e97d11a5bfeaabf8ded099db5420568
Gian Carlo Pace <giancarlo.pace@gmail.com>
2019-09-13 12:05:43 +0200

    Added mockito

--- cise-emulator-domain/src/test/java/eu/cise/emulator/EmulatorEngineTest.java
+++ cise-emulator-domain/src/test/java/eu/cise/emulator/EmulatorEngineTest.java
@@ -4,10 +4,22 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class EmulatorEngineTest {
     @Test
     public void it_xxx() {
         assertThat(1, is(1));
+    }
+
+    @Test
+    public void it_mocks() {
+        EmulatorEngine engine = mock(EmulatorEngine.class);
+
+        engine.send(null);
+
+        verify(engine).send(any());
     }
 }
