e69f3d05740763772304b84879a0d87a9d444446
Gian Carlo Pace <giancarlo.pace@gmail.com>
2019-09-13 23:36:35 +0200

    OCNET-154 RED: testing the message signature

---
+++ cise-emulator-domain/src/test/java/eu/cise/emulator/AddingSignatureTest.java
@@ -0,0 +1,34 @@
+package eu.cise.emulator;
+
+import eu.cise.servicemodel.v1.message.Push;
+import org.junit.Before;
+import org.junit.Test;
+
+import static eu.eucise.helpers.PushBuilder.newPush;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class AddingSignatureTest {
+
+    private EmulatorEngine engine;
+    private SignatureService signature;
+    private Push push;
+
+    @Before
+    public void before() {
+        signature = mock(SignatureService.class);
+        engine = new DefaultEmulatorEngine();
+        push = newPush().build();
+    }
+
+    @Test
+    public void it_signs_the_message() {
+        engine.prepare(push, params());
+
+        verify(signature).sign(push);
+    }
+
+    private SendParam params() {
+        return new SendParam(false, "msgId", "corrId");
+    }
+}
