bfc79d3976d04d77fba9da66cebf9f492d2803fe
Gian Carlo Pace <giancarlo.pace@gmail.com>
2019-09-13 12:01:43 +0200

    Added junit

---
+++ cise-emulator-domain/src/test/java/eu/cise/emulator/EmulatorEngineTest.java
@@ -0,0 +1,13 @@
+package eu.cise.emulator;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class EmulatorEngineTest {
+    @Test
+    public void it_xxx() {
+        assertThat(1, is(1));
+    }
+}
