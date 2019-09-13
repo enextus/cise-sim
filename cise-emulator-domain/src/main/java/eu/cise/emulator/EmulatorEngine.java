886a65e766d950981ce156a8631d715f7adfbc7f
Gian Carlo Pace <giancarlo.pace@gmail.com>
2019-09-13 12:27:36 +0200

    Documented the interface

--- cise-emulator-domain/src/main/java/eu/cise/emulator/EmulatorEngine.java
+++ cise-emulator-domain/src/main/java/eu/cise/emulator/EmulatorEngine.java
@@ -3,9 +3,29 @@ package eu.cise.emulator;
 import eu.cise.servicemodel.v1.message.Acknowledgement;
 import eu.cise.servicemodel.v1.message.Message;
 
+/**
+ * This is the engine of the emulator that holds all the main
+ * flows of business logic for preparing,  sending, receiving
+ * interpreting the received message and act on it appropriately
+ */
 public interface EmulatorEngine {
 
-    Acknowledgement send(Message message);
-
+    /**
+     * Prepare the message before sending it by modifying some
+     * properties of it
+     *
+     * @param message the template message
+     * @param param the object holding the parameters to be modified
+     * @return the modified message
+     */
     Message prepare(Message message, SendParam param);
+
+    /**
+     * It sends the message
+     *
+     * @param message message to be sent
+     * @return a sync acknowledgment received
+     * TODO specify the runtime exceptions to be captured by the client
+     */
+    Acknowledgement send(Message message);
 }
--- cise-emulator-domain/src/test/java/eu/cise/emulator/EmulatorEngineTest.java
+++ cise-emulator-domain/src/test/java/eu/cise/emulator/EmulatorEngineTest.java
@@ -2,16 +2,11 @@ package eu.cise.emulator;
 
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class EmulatorEngineTest {
-    @Test
-    public void it_xxx() {
-        assertThat(1).isEqualTo(1);
-    }
 
     @Test
     public void it_mocks() {
