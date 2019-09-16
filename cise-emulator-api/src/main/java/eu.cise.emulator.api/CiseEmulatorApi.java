6611945f87e0a4dd1ffedca2c143afde64a8090a
Jason Francois Gonin <jason.gonin@ext.ec.europa.eu>
2019-09-16 09:23:48 +0200

    OCNET-154 WIP #time 30mn clean up the api part to fix the build

--- cise-emulator-api/src/main/java/eu.cise.emulator.api/CiseEmulatorApi.java
+++ cise-emulator-api/src/main/java/eu.cise.emulator.api/CiseEmulatorApi.java
@@ -1,8 +1,6 @@
 package eu.cise.emulator.api;
 
-import eu.cise.emulator.CiseEmulatorApp;
-import eu.cise.emulator.app.resource.OutBoundRestServiceAPI;
-import eu.cise.emulator.app.transport.OutBoundWebSocketClient;
+import eu.cise.emulator.Configuration;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
@@ -16,7 +14,7 @@ public class CiseEmulatorApi extends Application<CiseEmulatorConf> {
 
     @Override
     public void initialize(final Bootstrap<CiseEmulatorConf> bootstrap) {
-        CiseEmulatorApp.init(bootstrap);
+//        CiseEmulatorApp.init(bootstrap);
 
     }
 
@@ -24,17 +22,17 @@ public class CiseEmulatorApi extends Application<CiseEmulatorConf> {
     public void run(final CiseEmulatorConf configuration,
                     final Environment environment) {
 
-        this.instanceID = configuration.buildInstance();
-        this.appConfiguration = new Configuration (configuration);
+       /* this.instanceID = configuration.buildInstance();*/
+       // this.appConfiguration = new Configuration (configuration);
 
 
 ////OUT : allow service to inform itself on the identity and option given by the configuration
-        final OutBoundRestServiceAPI outRestMessageService = new OutBoundRestServiceAPI(this.instanceID, configuration);
+       /* final  outRestMessageService = new OutBoundRestServiceAPI(this.instanceID, configuration);
         environment.jersey().register(outRestMessageService);
-
+*/
         ////OUT : allow service to inform itself on the identity and option given by the configuration
-        final OutBoundWebSocketClient outRestMessageClient = OutBoundWebSocketClient.build("8080"); //TODO set dynamic value to reflect via this.instanceID.getPort()
-    }
+    /*    final OutBoundWebSocketClient outRestMessageClient = OutBoundWebSocketClient.build("8080"); //TODO set dynamic value to reflect via this.instanceID.getPort()
+  */  }
 
 }
 
