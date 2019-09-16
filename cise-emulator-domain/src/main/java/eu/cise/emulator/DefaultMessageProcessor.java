f1dcde298a549daecba91b2359cc41e182c8e5ea
Giorgio Ricciuti <Giorgio-Massimo.RICCIUTI@ec.europa.eu>
2019-09-16 17:51:18 +0200

    OCNET-154: MessageProcessor send implementation

--- cise-emulator-domain/src/main/java/eu/cise/emulator/DefaultMessageProcessor.java
+++ cise-emulator-domain/src/main/java/eu/cise/emulator/DefaultMessageProcessor.java
@@ -5,22 +5,25 @@ import eu.cise.servicemodel.v1.message.Message;
 
 public class DefaultMessageProcessor implements MessageProcessor {
 
-    private SignatureService signatureService;
-    private EmuConfig config;
+    private final EmulatorEngine emulatorEngine;
+    private final SignatureService signatureService;
+    private final EmuConfig config;
 
-    public DefaultMessageProcessor(SignatureService signatureService, EmuConfig config) {
+    public DefaultMessageProcessor(EmulatorEngine emulatorEngine, SignatureService signatureService, EmuConfig config) {
         this.signatureService = signatureService;
         this.config = config;
+        this.emulatorEngine = emulatorEngine;
     }
 
     @Override
     public Message preview(Message message, SendParam param) {
-        EmulatorEngine emulatorEngine = new DefaultEmulatorEngine(signatureService, config);
         return emulatorEngine.prepare(message, param);
     }
 
     @Override
     public Acknowledgement send(Message message, SendParam param) {
-        return null;
+        Message preparedMessage = emulatorEngine.prepare(message, param);
+        Acknowledgement acknowledgement = emulatorEngine.send(preparedMessage, param);
+        return acknowledgement;
     }
 }
--- cise-emulator-domain/src/test/java/eu/cise/emulator/MessageProcessorTest.java
+++ cise-emulator-domain/src/test/java/eu/cise/emulator/MessageProcessorTest.java
@@ -5,6 +5,7 @@ import eu.cise.servicemodel.v1.message.Acknowledgement;
 import eu.cise.servicemodel.v1.message.Message;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.internal.matchers.Any;
 
 import java.time.Clock;
 import java.time.Instant;
@@ -15,26 +16,30 @@ import static eu.cise.servicemodel.v1.service.ServiceType.VESSEL_SERVICE;
 import static eu.eucise.helpers.PushBuilder.newPush;
 import static eu.eucise.helpers.ServiceBuilder.newService;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class MessageProcessorTest {
 
     private SignatureService signatureService;
     private EmuConfig config;
     private MessageProcessor messageProcessor;
+    private EmulatorEngine engine;
 
     @Before
     public void before() {
         signatureService = new FakeSignatureService();
         config = mock(EmuConfig.class);
-        messageProcessor = new DefaultMessageProcessor(signatureService, config);
+        engine = mock(EmulatorEngine.class);
+        messageProcessor = new DefaultMessageProcessor(engine, signatureService, config);
     }
 
     @Test
-    public void it_creates_a_preview_message () {
-        Message message= newPush().sender(newService().type(VESSEL_SERVICE)).build();
+    public void it_creates_a_preview_message() {
+        Message message = newPush().sender(newService().type(VESSEL_SERVICE)).build();
         SendParam param = new SendParam(true, "messageId", "correlationId");
+
+        when(engine.prepare(message, param)).thenReturn(message);
 
         Message previewMessage = messageProcessor.preview(message, param);
 
@@ -42,16 +47,27 @@ public class MessageProcessorTest {
     }
 
     @Test
-    public void it_calls_prepare_and_send_from_emulator_engine () {
-        Message message= newPush().sender(newService().type(VESSEL_SERVICE)).build();
+    public void it_calls_prepare_from_emulator_engine() {
+        Message message = newPush().sender(newService().type(VESSEL_SERVICE)).build();
         SendParam param = new SendParam(true, "messageId", "correlationId");
 
-        Acknowledgement acknowledgement = messageProcessor.send(message, param);
+        messageProcessor.send(message, param);
 
-        DefaultEmulatorEngine engine = mock(DefaultEmulatorEngine.class);
         verify(engine).prepare(message, param);
-        verify(engine).send(message, param);
     }
 
+    @Test
+    public void it_calls_send_from_emulator_engine() {
+        Message message = newPush().sender(newService().type(VESSEL_SERVICE)).build();
+        SendParam param = new SendParam(true, "messageId", "correlationId");
+        Message preparedMessage = newPush().sender(newService().type(VESSEL_SERVICE)).build();
+        preparedMessage.setCorrelationID("new_correlation_id");
+
+        when(engine.prepare(message, param)).thenReturn(preparedMessage);
+
+        messageProcessor.send(message, param);
+
+        verify(engine).send(preparedMessage, param);
+    }
 
 }
