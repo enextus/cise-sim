79d7ba58740304743c823d9fb91b93dc308f90a5
Gian Carlo Pace <giancarlo.pace@gmail.com>
2019-09-16 11:11:59 +0200

    OCNET-154 starting from an empty dropwizard instance within the api

--- cise-emulator-api/src/main/java/eu.cise.emulator.api/CiseEmulatorApi.java
+++ cise-emulator-api/src/main/java/eu.cise.emulator.api/CiseEmulatorApi.java
@@ -1,12 +1,10 @@
 package eu.cise.emulator.api;
 
-import eu.cise.emulator.Configuration;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class CiseEmulatorApi extends Application<CiseEmulatorConf> {
-    private Configuration appConfiguration;
 
     public static void main(final String[] args) throws Exception {
         new CiseEmulatorApi().run(args);
@@ -14,25 +12,13 @@ public class CiseEmulatorApi extends Application<CiseEmulatorConf> {
 
     @Override
     public void initialize(final Bootstrap<CiseEmulatorConf> bootstrap) {
-//        CiseEmulatorApp.init(bootstrap); (method to init with reference)
 
     }
 
     @Override
-    public void run(final CiseEmulatorConf configuration,
-                    final Environment environment) {
+    public void run(final CiseEmulatorConf configuration, final Environment environment) {
 
-       /* this.instanceID = configuration.buildInstance();*/
-       // this.appConfiguration = new Configuration (configuration);
-
-
-////OUT : allow service to inform itself on the identity and option given by the configuration
-       /* final  outRestMessageService = new OutBoundRestServiceAPI(this.instanceID, configuration);
-        environment.jersey().register(outRestMessageService);
-*/
-        ////OUT : allow service to inform itself on the identity and option given by the configuration
-    /*    final OutBoundWebSocketClient outRestMessageClient = OutBoundWebSocketClient.build("8080"); //TODO set dynamic value to reflect via this.instanceID.getPort()
-  */  }
+    }
 
 }
 
