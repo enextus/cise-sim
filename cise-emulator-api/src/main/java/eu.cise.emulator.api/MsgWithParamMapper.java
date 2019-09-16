77e4f29dac09af0ae65524f00bd597306e2a980a
Gian Carlo Pace <giancarlo.pace@gmail.com>
2019-09-16 18:13:01 +0200

    OCNET-154 Checkstyle fix

--- cise-emulator-api/src/main/java/eu.cise.emulator.api/MsgWithParamMapper.java
+++ cise-emulator-api/src/main/java/eu.cise.emulator.api/MsgWithParamMapper.java
@@ -2,11 +2,8 @@ package eu.cise.emulator.api;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import eu.cise.emulator.SendParam;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class MsgWithParamMapper {
-    private static final Logger LOGGER = LoggerFactory.getLogger(MsgWithParamMapper.class);
 
     public SendParam extractSendParams(JsonNode json) {
         String messageId = json.at("/params/message-id").textValue();
@@ -19,11 +16,3 @@ public class MsgWithParamMapper {
         return json.at("/message-template").textValue();
     }
 }
-
-
-// params.put("requires-ack", "false");
-//         params.put("message-id", "1234-123411-123411-1234");
-//         params.put("correlation-id", "7777-666666-666666-5555");
-//
-//         msgTemplateWithParamObject.put("message-template", "hash-msg-template");
-//         msgTemplateWithParamObject.set("params", params);
