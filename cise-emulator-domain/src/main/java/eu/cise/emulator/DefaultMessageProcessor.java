635a74cd807f75f18ae4e4ceebc8a0827f0ce2d8
Giorgio Ricciuti <Giorgio-Massimo.RICCIUTI@ec.europa.eu>
2019-09-16 16:51:20 +0200

    OCNET-154: MessageProcessor preview implementation

--- cise-emulator-domain/src/main/java/eu/cise/emulator/DefaultMessageProcessor.java
+++ cise-emulator-domain/src/main/java/eu/cise/emulator/DefaultMessageProcessor.java
@@ -3,8 +3,18 @@ package eu.cise.emulator;
 import eu.cise.servicemodel.v1.message.Message;
 
 public class DefaultMessageProcessor implements MessageProcessor {
+
+    private SignatureService signatureService;
+    private EmuConfig config;
+
+    public DefaultMessageProcessor(SignatureService signatureService, EmuConfig config) {
+        this.signatureService = signatureService;
+        this.config = config;
+    }
+
     @Override
     public Message preview(Message message, SendParam param) {
-        return null;
+        EmulatorEngine emulatorEngine = new DefaultEmulatorEngine(signatureService, config);
+        return emulatorEngine.prepare(message, param);
     }
 }
--- cise-emulator-domain/src/test/java/eu/cise/emulator/MessageProcessorTest.java
+++ cise-emulator-domain/src/test/java/eu/cise/emulator/MessageProcessorTest.java
@@ -1,20 +1,33 @@
 package eu.cise.emulator;
 
+import eu.cise.emulator.utils.FakeSignatureService;
 import eu.cise.servicemodel.v1.message.Message;
+import org.junit.Before;
 import org.junit.Test;
 
+import static eu.cise.servicemodel.v1.service.ServiceType.VESSEL_SERVICE;
 import static eu.eucise.helpers.PushBuilder.newPush;
+import static eu.eucise.helpers.ServiceBuilder.newService;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class MessageProcessorTest {
 
+    private SignatureService signatureService;
+    private EmuConfig config;
+
+    @Before
+    public void before() {
+        signatureService = new FakeSignatureService();
+        config = mock(EmuConfig.class);
+    }
+
     @Test
     public void it_creates_a_preview_message () {
-        MessageProcessor messageProcessor = new DefaultMessageProcessor();
+        MessageProcessor messageProcessor = new DefaultMessageProcessor(signatureService, config);
 
-        Message message= newPush().build();
+        Message message= newPush().sender(newService().type(VESSEL_SERVICE)).build();
         SendParam param = new SendParam(true, "messageId", "correlationId");
-
         Message previewMessage = messageProcessor.preview(message, param);
 
         assertThat(previewMessage).isNotNull();
