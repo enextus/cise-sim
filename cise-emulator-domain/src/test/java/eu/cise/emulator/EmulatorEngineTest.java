5cfef78cae8490259bde0463dc663e21ad63d82e
Maurizio Longari <maurizio.longari@ext.ec.europa.eu>
2019-09-17 16:52:08 +0200

    OCNET-154 RED handling exception in the emulator engine thrown by the dispatcher instance

--- cise-emulator-domain/src/test/java/eu/cise/emulator/EmulatorEngineTest.java
+++ cise-emulator-domain/src/test/java/eu/cise/emulator/EmulatorEngineTest.java
@@ -1,13 +1,18 @@
 package eu.cise.emulator;
 
 
+import static eu.cise.servicemodel.v1.message.AcknowledgementType.END_POINT_NOT_FOUND;
 import static eu.eucise.helpers.PushBuilder.newPush;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 import eu.cise.dispatcher.Dispatcher;
+import eu.cise.dispatcher.DispatcherException;
 import eu.cise.emulator.utils.FakeSignatureService;
+import eu.cise.servicemodel.v1.message.Acknowledgement;
 import eu.cise.servicemodel.v1.message.Message;
 import eu.cise.signature.SignatureService;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -25,16 +30,36 @@ public class EmulatorEngineTest {
         config = mock(EmuConfig.class);
         dispatcher = mock(Dispatcher.class);
         engine = new DefaultEmulatorEngine(signatureService, dispatcher, config);
+        when(config.endpointUrl()).thenReturn("endpointUrl");
+    }
+
+    @After
+    public void after() {
+        reset(config);
     }
 
     @Test
-    public void it_send_the_message() {
+    public void it_sends_message_successfully() {
         Message message =  newPush().build();
-
-        when(config.endpointUrl()).thenReturn("endpointUrl");
 
         engine.send(message);
 
         verify(dispatcher).send(message, "endpointUrl");
     }
+
+    @Test
+    public void it_sends_message_failing_the_dispatch() {
+        Message message =  newPush().build();
+
+        doThrow(dispatcherException()).when(dispatcher).send(any(), any());
+
+        Acknowledgement ack = engine.send(message);
+
+        assertThat(ack.getAckCode()).isEqualTo(END_POINT_NOT_FOUND);
+    }
+
+    private DispatcherException dispatcherException() {
+        return new DispatcherException("Error while connecting to address|" + config.endpointUrl(), null);
+    }
+
 }
