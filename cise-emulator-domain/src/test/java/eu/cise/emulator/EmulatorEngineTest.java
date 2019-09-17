19ea637e0780f368e78b849f8d470b669fa5a2a5
Maurizio Longari <maurizio.longari@ext.ec.europa.eu>
2019-09-17 17:20:52 +0200

    OCNET-154 RED handling the return of the acknowledgment

--- cise-emulator-domain/src/test/java/eu/cise/emulator/EmulatorEngineTest.java
+++ cise-emulator-domain/src/test/java/eu/cise/emulator/EmulatorEngineTest.java
@@ -1,20 +1,22 @@
 package eu.cise.emulator;
 
 
-import static eu.cise.servicemodel.v1.message.AcknowledgementType.END_POINT_NOT_FOUND;
-import static eu.eucise.helpers.PushBuilder.newPush;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
-
+import eu.cise.dispatcher.DispatchResult;
 import eu.cise.dispatcher.Dispatcher;
 import eu.cise.dispatcher.DispatcherException;
 import eu.cise.emulator.utils.FakeSignatureService;
 import eu.cise.servicemodel.v1.message.Acknowledgement;
-import eu.cise.servicemodel.v1.message.Message;
+import eu.cise.servicemodel.v1.message.Push;
 import eu.cise.signature.SignatureService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import static eu.cise.servicemodel.v1.message.AcknowledgementType.END_POINT_NOT_FOUND;
+import static eu.cise.servicemodel.v1.message.AcknowledgementType.SUCCESS;
+import static eu.eucise.helpers.PushBuilder.newPush;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 public class EmulatorEngineTest {
 
@@ -23,6 +25,7 @@ public class EmulatorEngineTest {
     private MessageProcessor messageProcessor;
     private EmulatorEngine engine;
     private Dispatcher dispatcher;
+    private Push message;
 
     @Before
     public void before() {
@@ -30,6 +33,9 @@ public class EmulatorEngineTest {
         config = mock(EmuConfig.class);
         dispatcher = mock(Dispatcher.class);
         engine = new DefaultEmulatorEngine(signatureService, dispatcher, config);
+        message = newPush().build();
+
+        when(config.serviceId()).thenReturn("service-id");
         when(config.endpointUrl()).thenReturn("endpointUrl");
     }
 
@@ -40,22 +46,45 @@ public class EmulatorEngineTest {
 
     @Test
     public void it_sends_message_successfully() {
-        Message message =  newPush().build();
-
         engine.send(message);
 
         verify(dispatcher).send(message, "endpointUrl");
     }
 
     @Test
-    public void it_sends_message_failing_the_dispatch() {
-        Message message =  newPush().build();
-
+    public void it_sends_a_message_failing_the_dispatch() {
         doThrow(dispatcherException()).when(dispatcher).send(any(), any());
 
         Acknowledgement ack = engine.send(message);
 
         assertThat(ack.getAckCode()).isEqualTo(END_POINT_NOT_FOUND);
+    }
+
+    @Test
+    public void it_returns_a_sync_ack_with_an_ack_code_successful() {
+        when(dispatcher.send(message, config.endpointUrl())).thenReturn(DispatchResult.success());
+
+        Acknowledgement ack = engine.send(message);
+
+        assertThat(ack.getAckCode()).isEqualTo(SUCCESS);
+    }
+
+    @Test
+    public void it_returns_a_sync_ack_with_a_sender_not_null() {
+        when(dispatcher.send(message, config.endpointUrl())).thenReturn(DispatchResult.success());
+
+        Acknowledgement ack = engine.send(message);
+
+        assertThat(ack.getSender()).isNotNull();
+    }
+
+    @Test
+    public void it_returns_a_sync_ack_with_a_sender_serviceId() {
+        when(dispatcher.send(message, config.endpointUrl())).thenReturn(DispatchResult.success());
+
+        Acknowledgement ack = engine.send(message);
+
+        assertThat(ack.getSender().getServiceID()).isEqualTo("service-id");
     }
 
     private DispatcherException dispatcherException() {
