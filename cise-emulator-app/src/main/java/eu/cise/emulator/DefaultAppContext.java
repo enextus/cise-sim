d8147c24bc0d6e9f7f2a072eca3241347a63706a
Maurizio Longari <maurizio.longari@ext.ec.europa.eu>
2019-09-17 12:29:16 +0200

    OCNET-154 GREEN the AppContext now returns a MessageProcessor valued

--- cise-emulator-app/src/main/java/eu/cise/emulator/DefaultAppContext.java
+++ cise-emulator-app/src/main/java/eu/cise/emulator/DefaultAppContext.java
@@ -1,8 +1,30 @@
 package eu.cise.emulator;
 
+import eu.cise.signature.SignatureService;
+import eu.cise.signature.SignatureServiceBuilder;
+import org.aeonbits.owner.ConfigFactory;
+
+import static eu.cise.signature.SignatureServiceBuilder.newSignatureService;
+
 public class DefaultAppContext implements AppContext {
+
+    private final EmuConfig emuConfig;
+
+    DefaultAppContext() {
+        this.emuConfig = ConfigFactory.create(EmuConfig.class);
+    }
+
     @Override
     public MessageProcessor makeMessageProcessor() {
-        return null;
+        return new DefaultMessageProcessor(new DefaultEmulatorEngine(makeSignatureService(), this.emuConfig), makeSignatureService(), this.emuConfig);
+    }
+
+    private SignatureService makeSignatureService() {
+        return newSignatureService()
+                .withKeyStoreName("adaptor.jks")
+                .withKeyStorePassword("eucise")
+                .withPrivateKeyAlias("sim1-node01.node01.eucise.fr")
+                .withPrivateKeyPassword("eucise")
+                .build();
     }
 }
--- cise-emulator-domain/src/main/java/eu/cise/emulator/exceptions/NullSignatureServiceEx.java
+++ cise-emulator-domain/src/main/java/eu/cise/emulator/exceptions/NullSignatureServiceEx.java
@@ -2,7 +2,7 @@ package eu.cise.emulator.exceptions;
 
 public class NullSignatureServiceEx extends RuntimeException {
 
-    private static final String EXCEPTION_MESSAGE = "The signature service parameter passed to the" +
+    private static final String EXCEPTION_MESSAGE = "The signature service parameter passed to the " +
             "emulator engine object is null";
 
     public NullSignatureServiceEx() {
