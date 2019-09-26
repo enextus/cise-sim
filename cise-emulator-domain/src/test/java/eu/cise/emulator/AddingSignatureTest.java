0cb10ff02e1518cf5501d72d23f204a465a0adf7
Giorgio Ricciuti <Giorgio-Massimo.RICCIUTI@ec.europa.eu>
2019-09-26 10:47:55 +0200

    OCNET-161 GREEN new test verify signature

--- cise-emulator-domain/src/test/java/eu/cise/emulator/AddingSignatureTest.java
+++ cise-emulator-domain/src/test/java/eu/cise/emulator/AddingSignatureTest.java
@@ -1,16 +1,23 @@
 package eu.cise.emulator;
 
+import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl;
 import eu.cise.dispatcher.Dispatcher;
 import eu.cise.servicemodel.v1.message.Push;
+import eu.cise.servicemodel.v1.service.ServiceType;
 import eu.cise.signature.SignatureService;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.xml.datatype.XMLGregorianCalendar;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import static eu.eucise.helpers.PushBuilder.newPush;
 import static eu.eucise.helpers.ServiceBuilder.newService;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class AddingSignatureTest {
 
@@ -18,6 +25,7 @@ public class AddingSignatureTest {
     private SignatureService signatureService;
     private Push push;
     private Dispatcher dispatcher;
+    private EmuConfig config = mock(EmuConfig.class);
 
     @Before
     public void before() {
@@ -26,6 +34,7 @@ public class AddingSignatureTest {
         dispatcher = mock(Dispatcher.class);
         engine = new DefaultEmulatorEngine(signatureService, dispatcher, config);
         push = newPush().sender(newService()).build();
+        push.setCreationDateTime(getUTCDatetime());
     }
 
     @Test
@@ -35,7 +44,23 @@ public class AddingSignatureTest {
         verify(signatureService).sign(push);
     }
 
+    @Test
+    public void it_verify_the_signature() {
+        when(config.serviceType()).thenReturn(ServiceType.VESSEL_SERVICE);
+        push.getSender().setServiceType(config.serviceType());
+
+        engine.receive(push);
+
+        verify(signatureService).verify(push);
+    }
+
     private SendParam params() {
         return new SendParam(false, "msgId", "corrId");
+    }
+
+    private XMLGregorianCalendar getUTCDatetime() {
+        GregorianCalendar datetime = new GregorianCalendar();
+        datetime.setTime(java.util.Date.from(ZonedDateTime.now(ZoneId.of("UTC")).toInstant()));
+        return new XMLGregorianCalendarImpl(datetime);
     }
 }
