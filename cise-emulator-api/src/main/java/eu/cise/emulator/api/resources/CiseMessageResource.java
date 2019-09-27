9288a73d5510d0741f5f3c7fd23729811a5e8cba
Maurizio Longari <maurizio.longari@ext.ec.europa.eu>
2019-09-27 15:55:34 +0200

    OCNET-161 fix for bugs in passing the storage to the resource and for the annotation of the resource

--- cise-emulator-api/src/main/java/eu/cise/emulator/api/resources/CiseMessageResource.java
+++ cise-emulator-api/src/main/java/eu/cise/emulator/api/resources/CiseMessageResource.java
@@ -14,6 +14,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+@Path("/api/cisemessages")
 public class CiseMessageResource {
     public static final String ERROR = "ERROR";
     public static final String ACK = "ACK";
@@ -34,7 +35,6 @@ public class CiseMessageResource {
     @POST
     @Consumes("text/plain,text/xml,application/xml")
     @Produces("text/xml")
-    @Path("/api/cisemessage")
     public Response receive(String inputXmlMessage) {
         Acknowledgement acknowledgement = messageAPI.receive(inputXmlMessage);
 
