1621ba199a05c38cc7d36e8dbb4ac83f8fc6a8a1
Giorgio Ricciuti <Giorgio-Massimo.RICCIUTI@ec.europa.eu>
2019-09-27 12:15:34 +0200

    OCNET-161 GREEN: implements the message store once received the message from the node

--- cise-emulator-api/src/main/java/eu/cise/emulator/api/MessageApiDto.java
+++ cise-emulator-api/src/main/java/eu/cise/emulator/api/MessageApiDto.java
@@ -1,13 +1,10 @@
 package eu.cise.emulator.api;
 
 
-//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
 
-//@JsonIgnoreProperties(ignoreUnknown = true)
 public class MessageApiDto implements Serializable {
 
     @JsonProperty("status")
--- cise-emulator-api/src/main/java/eu/cise/emulator/api/resources/CiseMessageResource.java
+++ cise-emulator-api/src/main/java/eu/cise/emulator/api/resources/CiseMessageResource.java
@@ -1,6 +1,7 @@
 package eu.cise.emulator.api.resources;
 
 import eu.cise.emulator.api.MessageAPI;
+import eu.cise.emulator.api.MessageApiDto;
 import eu.cise.io.MessageStorage;
 import eu.cise.servicemodel.v1.message.Acknowledgement;
 import eu.eucise.xml.DefaultXmlMapper;
@@ -19,6 +20,7 @@ public class CiseMessageResource {
     public static final String MESSAGE_MODAL = "RECEIVE";
     private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(CiseMessageResource.class);
     private final MessageAPI messageAPI;
+    private final MessageStorage messageStorage;
 
     private String fileNameTemplate;
     private XmlMapper xmlMapper = new DefaultXmlMapper();
@@ -26,6 +28,7 @@ public class CiseMessageResource {
 
     public CiseMessageResource(MessageAPI messageAPI, MessageStorage messageStorage) {
         this.messageAPI = messageAPI;
+        this.messageStorage = messageStorage;
     }
 
     @POST
@@ -34,6 +37,16 @@ public class CiseMessageResource {
     @Path("/api/cisemessage")
     public Response receive(String inputXmlMessage) {
         Acknowledgement acknowledgement = messageAPI.receive(inputXmlMessage);
+
+        // store the input message and the acknowledgement
+        XmlMapper xmlMapper = new DefaultXmlMapper.NotValidating();
+        String acknowledgementXml = xmlMapper.toXML(acknowledgement);
+        MessageApiDto messageApiDto = new MessageApiDto(Response.Status.CREATED.getStatusCode(),
+                "",
+                acknowledgementXml,
+                inputXmlMessage);
+
+        messageStorage.store(messageApiDto);
 
         return Response
                 .status(Response.Status.CREATED)
