758385f090bac9abded1d704dc6c1f62a5e52c96
Gian Carlo Pace <giancarlo.pace@gmail.com>
2019-10-14 21:29:53 +0200

    fixing the receive

--- cise-emulator-react/src/js/components/Body.js
+++ cise-emulator-react/src/js/components/Body.js
@@ -16,8 +16,7 @@ export default class Body extends Component {
             <Grid container spacing={2}>
                 <Grid item xs={12}>
                     <SendForm
-                        store={this.props.store}
-                        messageCandidate={messageCandidate}/>
+                        store={this.props.store}/>
                     <PreviewMessage
                         store={this.props.store}/>
                     <PushedMessage
--- cise-emulator-react/src/js/components/PushedMessage.js
+++ cise-emulator-react/src/js/components/PushedMessage.js
@@ -1,13 +1,6 @@
 import React, {Component} from "react";
 import {observer} from "mobx-react";
-import {
-  ExpansionPanel,
-  ExpansionPanelDetails,
-  ExpansionPanelSummary,
-  Tab,
-  Tabs,
-  Typography
-} from '@material-ui/core';
+import {ExpansionPanel, ExpansionPanelDetails, ExpansionPanelSummary, Tab, Tabs, Typography} from '@material-ui/core';
 import {withStyles} from '@material-ui/core/styles';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import PropTypes from 'prop-types';
@@ -15,74 +8,74 @@ import XmlContent from "./common/XmlContent";
 import TabPanel from "./common/TabPanel";
 
 const style = (theme) => ({
-  root: {
-    padding: theme.spacing(1),
-  },
+    root: {
+        padding: theme.spacing(1),
+    },
 });
 
 @observer
 class PushedMessage extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {tabValue: 'one'};
-    this.handleChange = this.handleChange.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {tabValue: 'one'};
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-  handleChange(event, newValue) {
-    this.setState({tabValue: newValue})
-  };
+    handleChange(event, newValue) {
+        this.setState({tabValue: newValue})
+    };
 
-  render() {
-    const {classes} = this.props;
-    return (
-        <div className={classes.root}>
-          <ExpansionPanel expanded={this.getMessageStore().sentMessage.body !== ""}>
-            <ExpansionPanelSummary
-                expandIcon={<ExpandMoreIcon/>}
-                aria-controls="SentMessageContent"
-                id="SentMessage">
-              <Typography variant="h6">Message Sent</Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              <Tabs
-                  value={this.state.tabValue}
-                  onChange={this.handleChange}
-                  aria-label="simple-tabpanel"
-                  indicatorColor="primary"
-                  textColor="primary">
-                <Tab value="one"
-                    label="message"
-                    id='simple-tab-2'
-                    aria-controls='simple-tabpanel-2'/>
-                <Tab value="two"
-                    label="acknowledgement"
-                     id='simple-tab-3'
-                     aria-controls='simple-tabpanel-3'/>
-              </Tabs>
-              <TabPanel index="one" value={this.state.tabValue}>
-                <XmlContent>
-                  {this.getMessageStore().sentMessage.body}
-                </XmlContent>
-              </TabPanel>
-              <TabPanel index="two" value={this.state.tabValue}>
-                <XmlContent>
-                  {this.getMessageStore().sentMessage.acknowledge}
-                </XmlContent>
-              </TabPanel>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-        </div>
-    );
-  }
+    render() {
+        const {classes} = this.props;
+        return (
+            <div className={classes.root}>
+                <ExpansionPanel expanded={this.getMessageStore().sentMessage.body !== ""}>
+                    <ExpansionPanelSummary
+                        expandIcon={<ExpandMoreIcon/>}
+                        aria-controls="SentMessageContent"
+                        id="SentMessage">
+                        <Typography variant="h6">Message Sent</Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <Tabs
+                            value={this.state.tabValue}
+                            onChange={this.handleChange}
+                            aria-label="simple-tabpanel"
+                            indicatorColor="primary"
+                            textColor="primary">
+                            <Tab value="one"
+                                 label="message"
+                                 id='simple-tab-2'
+                                 aria-controls='simple-tabpanel-2'/>
+                            <Tab value="two"
+                                 label="acknowledgement"
+                                 id='simple-tab-3'
+                                 aria-controls='simple-tabpanel-3'/>
+                        </Tabs>
+                        <TabPanel index="one" value={this.state.tabValue}>
+                            <XmlContent>
+                                {this.getMessageStore().sentMessage.body}
+                            </XmlContent>
+                        </TabPanel>
+                        <TabPanel index="two" value={this.state.tabValue}>
+                            <XmlContent>
+                                {this.getMessageStore().sentMessage.acknowledge}
+                            </XmlContent>
+                        </TabPanel>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+            </div>
+        );
+    }
 
-  getMessageStore() {
-    return this.props.store.messageStore;
-  }
+    getMessageStore() {
+        return this.props.store.messageStore;
+    }
 }
 
 PushedMessage.propTypes = {
-  classes: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired
 };
 
 export default withStyles(style)(PushedMessage)
