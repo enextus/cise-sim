4c1ea986a1d4d6cf52f809e309172589365d7a69
Gian Carlo Pace <giancarlo.pace@ec.europa.eu>
2019-10-22 17:36:31 +0200

    Deleted unused java keystores and fixed minor issues for the build

--- cise-emulator-app/src/test/java/eu/cise/emulator/AppContextTest.java
+++ cise-emulator-app/src/test/java/eu/cise/emulator/AppContextTest.java
@@ -1,14 +1,9 @@
 package eu.cise.emulator;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import eu.eucise.xml.XmlMapper;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 
 public class AppContextTest {
 
