05e5925013b92398938fe2096070c03eb04f51aa
Jason Francois Gonin <jason.gonin@ext.ec.europa.eu>
2019-10-30 17:23:47 +0100

    OCNET-163 create (duplicate) the solution found in emulator cli

---
+++ cise-emulator-api/java-templates/eu/cise/accesspoint/service/v1/SendResponse.java
@@ -0,0 +1,63 @@
+
+package eu.cise.accesspoint.service.v1;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+import eu.cise.servicemodel.v1.message.Acknowledgement;
+
+
+/**
+ * <p>Java class for sendResponse complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="sendResponse"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="return" type="{http://www.cise.eu/servicemodel/v1/message/}Acknowledgement" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "sendResponse", propOrder = {
+    "_return"
+})
+public class SendResponse {
+
+    @XmlElement(name = "return")
+    protected Acknowledgement _return;
+
+    /**
+     * Gets the value of the return property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Acknowledgement }
+     *     
+     */
+    public Acknowledgement getReturn() {
+        return _return;
+    }
+
+    /**
+     * Sets the value of the return property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Acknowledgement }
+     *     
+     */
+    public void setReturn(Acknowledgement value) {
+        this._return = value;
+    }
+
+}
--- cise-emulator-api/src/main/java/eu/cise/emulator/api/EmulatorApp.java
+++ cise-emulator-api/src/main/java/eu/cise/emulator/api/EmulatorApp.java
@@ -1,21 +1,27 @@
 package eu.cise.emulator.api;
 
 import com.codahale.metrics.health.HealthCheck;
+import com.roskart.dropwizard.jaxws.EndpointBuilder;
+import com.roskart.dropwizard.jaxws.JAXWSBundle;
 import eu.cise.emulator.AppContext;
 import eu.cise.emulator.DefaultAppContext;
 import eu.cise.emulator.api.helpers.CrossOriginSupport;
-import eu.cise.emulator.api.resources.MessageResource;
-import eu.cise.emulator.api.resources.TemplateResource;
-import eu.cise.emulator.api.resources.UiMessageResource;
-import eu.cise.emulator.api.resources.UiServiceResource;
+import eu.cise.emulator.api.resources.*;
 import eu.cise.emulator.io.MessageStorage;
 import eu.eucise.xml.XmlMapper;
 import io.dropwizard.Application;
 import io.dropwizard.bundles.assets.ConfiguredAssetsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.apache.cxf.interceptor.LoggingInInterceptor;
+import org.apache.cxf.interceptor.LoggingOutInterceptor;
+
+import javax.xml.ws.Endpoint;
 
 public class EmulatorApp extends Application<EmulatorConf> {
+
+    // JAX-WS Bundle
+    private static final JAXWSBundle<Object> JAXWS_BUNDLE = new JAXWSBundle<>("/api/soap");
 
     public static void main(final String[] args) throws Exception {
         System.out.println("\n==============================================");
@@ -24,9 +30,10 @@ public class EmulatorApp extends Application<EmulatorConf> {
 
     @Override
     public void initialize(final Bootstrap<EmulatorConf> bootstrap) {
+        bootstrap.addBundle(JAXWS_BUNDLE);
         bootstrap.addBundle(
-            new ConfiguredAssetsBundle("/assets/", "/",
-                "index.html")); // imply redirect from root ?
+                new ConfiguredAssetsBundle("/assets/", "/",
+                        "index.html")); // imply redirect from root ?
     }
 
     @Override
@@ -34,16 +41,22 @@ public class EmulatorApp extends Application<EmulatorConf> {
         CrossOriginSupport.setup(environment);
 
         environment.jersey().setUrlPattern("/api");
+        //if (configuration.getTransportMode().toUpperCase().contains("SOAP")  ) { // WSDL first service using server side JAX-WS handler and CXF logging interceptors
+            Endpoint e = JAXWS_BUNDLE.publishEndpoint(
+                    new EndpointBuilder("messages", new CISEMessagesSOAPServiceImpl())
+                            .cxfInInterceptors(new LoggingInInterceptor())
+                            .cxfOutInterceptors(new LoggingOutInterceptor()));
+        //}
 
         AppContext appCtx = new DefaultAppContext();
         XmlMapper xmlMapper = appCtx.getXmlMapper();
         MessageStorage messageStorage = appCtx.makeMessageStorage();
         MessageAPI messageAPI = new DefaultMessageAPI(
-            appCtx.makeMessageProcessor(),
-            messageStorage,
-            appCtx.makeTemplateLoader(),
-            xmlMapper,
-            appCtx.getPrettyNotValidatingXmlMapper());
+                appCtx.makeMessageProcessor(),
+                messageStorage,
+                appCtx.makeTemplateLoader(),
+                xmlMapper,
+                appCtx.getPrettyNotValidatingXmlMapper());
 
         environment.healthChecks().register("noop", new HealthCheck() {
             @Override
@@ -55,11 +68,11 @@ public class EmulatorApp extends Application<EmulatorConf> {
         environment.jersey().register(new UiServiceResource(appCtx.makeEmuConfig()));
         environment.jersey().register(new MessageResource(messageAPI, messageStorage));
         environment.jersey().register(
-            new TemplateResource(messageAPI,
-                new TemplateAPI(
-                    appCtx.makeMessageProcessor(),
-                    appCtx.makeTemplateLoader(),
-                    xmlMapper, appCtx.getPrettyNotValidatingXmlMapper())));
+                new TemplateResource(messageAPI,
+                        new TemplateAPI(
+                                appCtx.makeMessageProcessor(),
+                                appCtx.makeTemplateLoader(),
+                                xmlMapper, appCtx.getPrettyNotValidatingXmlMapper())));
 
         environment.lifecycle().addServerLifecycleListener(server -> {
             System.out.println("==============================================\n");
