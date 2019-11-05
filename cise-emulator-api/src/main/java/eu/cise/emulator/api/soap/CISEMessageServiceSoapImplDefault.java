c4c1fd5a2fab5d2a47c7cb24ceaab9835acb651d
Jason Francois Gonin <jason.gonin@ext.ec.europa.eu>
2019-11-05 16:02:13 +0100

    OCNET-163 fix the message validation  when receive xml message with ccRecipient from pull unknow

--- cise-emulator-api/src/main/java/eu/cise/emulator/api/soap/CISEMessageServiceSoapImplDefault.java
+++ cise-emulator-api/src/main/java/eu/cise/emulator/api/soap/CISEMessageServiceSoapImplDefault.java
@@ -1,9 +1,10 @@
-package eu.cise.emulator.api.resources;
+package eu.cise.emulator.api.soap;
+
+import eu.cise.accesspoint.service.v1.CISEMessageServiceSoapImpl;
 import eu.cise.emulator.api.MessageAPI;
 import eu.cise.emulator.io.MessageStorage;
 import eu.cise.servicemodel.v1.message.Acknowledgement;
 import eu.cise.servicemodel.v1.message.Message;
-import eu.cise.accesspoint.service.v1.CISEMessageServiceSoapImpl;
 import eu.eucise.xml.XmlMapper;
 
 import javax.jws.WebService;
@@ -15,20 +16,20 @@ import javax.jws.WebService;
         wsdlLocation = "META-INF/CISEMessageService.wsdl")*/
 @WebService(serviceName = "CISEMessageService", portName = "CISEMessageServiceSoapPort",
         endpointInterface = "eu.cise.accesspoint.service.v1.CISEMessageServiceSoapImpl", targetNamespace = "http://www.cise.eu/accesspoint/service/v1/", wsdlLocation = "META-INF/CISEMessageService.wsdl")
-//@HandlerChain(file = "WsChainhandler.xml")
+
 public class CISEMessageServiceSoapImplDefault implements CISEMessageServiceSoapImpl {
     private static MessageAPI messageAPI;
-    private static  MessageStorage messageStorage;
+    private static MessageStorage messageStorage;
     private static XmlMapper xmlMapper;
 
-/* TODO :  improve the implementation by following the bundle life cycle instead of post creation static variable setting */
+    /* TODO :  improve the implementation by following the bundle life cycle instead of post creation static variable setting */
     public static void setMessageAPI(MessageAPI messageAPI) {
         CISEMessageServiceSoapImplDefault.messageAPI = messageAPI;
     }
+
     public static void setXmlMapper(XmlMapper xmlMapper) {
         CISEMessageServiceSoapImplDefault.xmlMapper = xmlMapper;
     }
-
 
 
     public CISEMessageServiceSoapImplDefault(MessageAPI messageAPI, XmlMapper xmlMapper) {
