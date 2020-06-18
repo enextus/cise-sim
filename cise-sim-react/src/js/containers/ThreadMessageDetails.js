8d89d735e21f71ea7f3be65278e46f058d1fdbdf
Alessandro La Porta <Alessandro.LA-PORTA@ext.ec.europa.eu>
2020-06-18 10:09:40 +0200

    OCNET-386 Added suggestion on the empty thread details page

--- cise-sim-react/src/js/containers/ThreadMessageDetails.js
+++ cise-sim-react/src/js/containers/ThreadMessageDetails.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
-import {Box, Grid} from '@material-ui/core';
+import {Box, Grid, Typography} from '@material-ui/core';
 import {withStyles} from '@material-ui/core/styles';
 import {observer} from "mobx-react";
 import MessageInfoCard from "../forms/MessageForm/MessageInfoCard";
 import Slide from "@material-ui/core/Slide";
+import Logo from "./message-detail-icon.jpg"
+import TableContainer from "@material-ui/core/TableContainer";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
 
 const styles = theme => ({
     root: {
@@ -37,6 +43,49 @@ class ThreadMessageDetails extends Component {
         });
     }
 
+    buildList(classes, messageList) {
+
+        return ( <Slide  direction="right" in={messageList.length>0} unmountOnExit>
+                <Grid container className={classes.root}>
+                    {messageList.map((msg) =>
+                        <MessageInfoCard
+                            key={msg.msgInfo.id}
+                            msgInfo={msg.msgInfo}
+                            body={msg.body}
+                        />
+                    )}
+                </Grid>
+            </Slide>
+
+        )
+    }
+
+    buildWelcome(classes) {
+
+        return (
+            <TableContainer>
+                <Table size="small" aria-label="a dense table" style={{marginTop:200}}>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell style={{borderBottom:0}}>
+                                <p align="center">
+                                <img src={Logo}  width="100" height="100" alt="Select a thread to see details" />
+                                </p>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell style={{borderBottom:0}}>
+                                <Typography align="center" variant="h5">Select a Thread to see details</Typography>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+            )
+    }
+
+
     render() {
 
         const {classes} = this.props;
@@ -44,23 +93,18 @@ class ThreadMessageDetails extends Component {
 
         // toDo the ordering by timestamp
         //messageList.sort(function(a,b) {return b.msgInfo.dateTime-a.msgInfo.dateTime});
-
-        this.buildAckSuccessFail(messageList);
+        let showThreadDetails;
+        if (messageList.length === 0) {
+            showThreadDetails = this.buildWelcome(classes);
+        }
+        else {
+            this.buildAckSuccessFail(messageList);
+            showThreadDetails = this.buildList(classes, messageList);
+        }
 
         return (
-            <Box p="8px" mt="20px" mx="58px" hidden={messageList.length === 0}>
-                <Slide  direction="right" in={messageList.length>0} unmountOnExit>
-                    <Grid container className={classes.root}>
-                        {messageList.map((msg) =>
-                            <MessageInfoCard
-                                key={msg.msgInfo.id}
-                                msgInfo={msg.msgInfo}
-                                body={msg.body}
-                            />
-                        )}
-
-                    </Grid>
-                </Slide>
+            <Box p="8px" mt="20px" mx="58px">
+                {showThreadDetails}
             </Box>
         )
     }
